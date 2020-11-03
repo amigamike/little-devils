433ded537c190db9f64ccc93161e974940e381b1
Mike Welsh <mike@amigamike.com>
2020-11-03 17:08:45 +0000

    sql updates

--- templates/common/footer.php
+++ templates/common/footer.php
@@ -1,3 +1,8 @@
+            <div class="row">
+                <div class="col-12 mb-2">
+                    <small class="pl-3">&copy;<?= date('Y'); ?> Mike Welsh</small>
+                </div>
+            </div>
         </div>
         <script type="text/javascript">
             $('.datepicker').datepicker({
